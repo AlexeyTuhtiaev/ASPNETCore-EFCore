e4292d4d0ddd84da1b6362657809547d0b4fa865
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-04-07 20:28:09 +0300

    Entity + EFContext

---
+++ WorkWithEFCoreApp/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WorkWithEFCoreApp.Entities;
+
+namespace WorkWithEFCoreApp
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-L6BKAANE\MSSQL2014DEVEDIT; Database=EfCoreDB; Trusted_Connection=True;");
+        }
+    }
+}
