394d686a9e887fa87c8d4cdd8a1faeca416f9cd0
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-04-07 20:11:12 +0300

    console app

---
+++ WorkWithEFCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkWithEFCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
